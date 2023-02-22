0a206b9b5bec5ffadf5ec178c1d56d7530718475
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2023-02-22 00:21:22 -0500

    Add baseline "using move" code

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ log_data = log.read()
 log_arr = log_data.split('\n')
 
 # Compiling regex patterns for efficiency
-battle_started_pat = re.compile(r"Battle between .* and .* (started)|(is underway)!", re.IGNORECASE)
+battle_started_pat = re.compile(r"Battle between .* and .* (started)|(is underway)!",
+                                re.IGNORECASE)
 mode_pat = re.compile(r"Mode: \w*", re.IGNORECASE)
 tier_pat = re.compile(r"Tier: \w*", re.IGNORECASE)
 turn_start_pat = re.compile(r"Start of turn \d*!?", re.IGNORECASE)
@@ -22,20 +23,21 @@ move_used_pat = re.compile(r".* used .*!")
 is_watching_pat = re.compile(r".* is watching the battle.")
 stopped_watching_pat = re.compile(r".* stopped watching the battle.")
 chat_pat = re.compile(r".*: .*")
+win_battle = re.compile(r".* won the battle!")
 
-battle_started_line = 0
-mode_line = 0
-tier_line = 0
-turn_start_lines = []
-clause_lines = []
-sent_out_lines = []
 move_used_lines = []
-is_watching_lines = []
-stopped_watching_lines = []
-chat_lines = []
 
+current_player = -1
 gen = 0
 players = []
+
+
+# Function that defines how I output each line
+# As of now I just output to standard output
+# At some point I will send input to a file
+def output(str):
+    print(str)
+
 
 for line_num in range(len(log_arr)):
     line = log_arr[line_num]
@@ -55,32 +57,39 @@ for line_num in range(len(log_arr)):
         # Is it "Jeff and Bob" vs "Alice and Willy"?
         # idk and idc
         converted = f"|player|p1|{players[0].name}|ethan|"
-        print(converted)
+        output(converted)
         converted = f"|player|p2|{players[1].name}|ethan|"
-        print(converted)
+        output(converted)
 
         # Eventually we want to be able to change these avatars,
         # will want to figure out an interface to do so
     if mode_pat.search(line) is not None:
         converted = f"|gametype|{line.replace('Mode: ', '', 1).lower()}"
-        print(converted)
+        output(converted)
+    
     elif tier_pat.match(line) is not None:
         tier_log = line
         gen = utils.get_gen(tier_log)
         tier = utils.get_tier(tier_log)
         converted = f"|gen|{gen}"
-        print(converted)
+        output(converted)
         converted = f"|tier|[Gen {gen}] {tier}"
-        print(converted)
+        output(converted)
+    
     elif clause_pat.match(line) is not None:
         converted = line.replace('Rule: ', '|rule|')
-        print(converted)
+        output(converted)
+    
     elif turn_start_pat.match(line) is not None:
+        if (players[0].currentmon is None or players[1].currentmon is None):
+            print('Error: unable to determine leads.', file=sys.stderr)
+            sys.exit(1)
         converted = line.replace('Start of turn ', '|turn|')
-        print(converted)
+        output(converted)
+
     elif sent_out_pat.match(line) is not None:
         if players[0].currentmon is None and players[1].currentmon is None:
-            print('|start')
+            output('|start')
         sent_out_data = line.split(' sent out ', 1)
         if (re.search('! \\(.*\\)', sent_out_data[1]) is not None):
             sent_out_data[1] = sent_out_data[1].split('! (', 1)
@@ -99,21 +108,86 @@ for line_num in range(len(log_arr)):
             player.add_pokemon(sent_out_data[1], sent_out_data[2])
         mon = player.get_pokemon(sent_out_data[1], sent_out_data[2])
         player.currentmon = mon
-        print(f'|switch|p{playernum + 1}a: {mon.nick}|{mon.species}|{mon.hp}/100')
+        output(f'|switch|p{playernum + 1}a: {mon.nick}|{mon.species}|{mon.hp}/100')
+
     elif move_used_pat.match(line) is not None:
+        if (re.match('The foe\'s .* used .*!', line) is not None):
+            # Have to figure out who "the foe" is
+            line = line.replace('The foe\'s ', '', 1)
+            line_components = line.split(' used ', 1)
+            line_components[1] = line_components[1].replace("!", "")
+            if (current_player == -1):
+                if (line_components[0].lower() == players[0].currentmon.nick.lower()
+                        and line_components[0].lower() != players[1].currentmon.nick.lower()):
+                    # Player 0 is the foe, so Player 1 is the current player
+                    current_player = 1
+                elif (line_components[0].lower() == players[1].currentmon.nick.lower()
+                        and line_components[0].lower() != players[0].currentmon.nick.lower()):
+                    # Player 1 is the foe, so Player 0 is the current player
+                    current_player = 0
+                else:
+                    print('Error: indistinguishable who is who from the given information.', file = sys.stderr)
+                    sys.exit(1)
+            other_player = int(not current_player)
+            converted = f'|move|p{other_player + 1}a: {players[other_player].currentmon.nick}|{line_components[1]}|p{current_player + 1}a: {players[current_player].currentmon.nick}'
+            output(converted)
+        elif (re.match(f'{players[0].name}\'s .* used .*!', line,
+                       re.IGNORECASE) is not None):
+            # Ok, p1 did it
+            line = re.sub(f'{players[0].name}\'s ', '', line)
+            line_components = line.split(' used ', 1)
+            output(line_components)
+            line_components[1] = line_components[1].replace("!", "")
+            converted = f'|move|p1a: {players[0].currentmon.nick}|{line_components[1]}|p2a: {players[1].currentmon.nick}'
+            output(converted)
+        elif (re.match(f'{players[0].name}\'s .* used .*!', line,
+                       re.IGNORECASE) is not None):
+            # Ok, p2 did it
+            line = re.sub(f'{players[0].name}\'s ', '', line)
+            line_components = line.split(' used ', 1)
+            line_components[1] = line_components[1].replace("!", "")
+            output(line_components)
+            converted = f'|move|p2a: {players[1].currentmon.nick}|{line_components[1]}|p1a: {players[0].currentmon.nick}'
+            output(converted)
+        else:
+            # Have to figure out who "the foe" isn't
+            line = line.replace('The foe\'s ', '', 1)
+            line_components = line.split(' used ', 1)
+            line_components[1] = line_components[1].replace("!", "")
+            if (current_player == -1):
+                if (line_components[0].lower() == players[0].currentmon.nick.lower()
+                        and line_components[0].lower() != players[1].currentmon.nick.lower()):
+                    # Player 0 is the current player
+                    current_player = 0
+                elif (line_components[0].lower() == players[1].currentmon.nick.lower()
+                        and line_components[0].lower() != players[0].currentmon.nick.lower()):
+                    # Player 1 is the current player
+                    current_player = 1
+            other_player = int(not current_player)
+            converted = f'|move|p{current_player + 1}a: {players[current_player].currentmon.nick}|{line_components[1]}|p{other_player + 1}a: {players[other_player].currentmon.nick}'
+            output(converted)
+        
         move_used_lines.append(line)
+        # TODO: Implement damage, secondary effects, etc. of moves
+
     elif is_watching_pat.match(line) is not None:
         converted = f"|j|{line.replace(' is watching the battle.', '')}"
-        print(converted)
+        output(converted)
+
     elif stopped_watching_pat.match(line) is not None:
         converted = f"|l|{line.replace(' stopped watching the battle.', '')}"
-        print(converted)
+        output(converted)
+
     elif chat_pat.match(line) is not None:
         full_msg = line.split(': ', 1)
         if (full_msg[0] == players[0].name or full_msg[0] == players[1].name):
             full_msg[0] = '☆' + full_msg[0]
         converted = f"|c|{full_msg[0]}|{full_msg[1]}"
-        print(converted)
+        output(converted)
+
+    elif win_battle.match(line) is not None:
+        converted = f"|win|{line.replace(' won the battle!', '')}"
+        output(converted)
 
 
 print(move_used_lines[0:5])
