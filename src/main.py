32c92d4fa34f8d5a0475388b2e1d720e8ba03ffd
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2023-08-11 22:04:33 -0700

    Added support for Sandstorm damage

--- src/main.py
+++ src/main.py
@@ -28,9 +28,9 @@ fainted_pat = re.compile(r"(.*) fainted!")
 
 stealth_rock_dmg_pat = re.compile(r'Pointed stones dug into (.*)!')
 spikes_dmg_pat = re.compile("(.*) (was|is) hurt by spikes!")
-burn_pat = re.compile("(.*) was hurt by its burn!")
-poison_pat = re.compile("(.*) was hurt by poison!")
-sandstorm_pat = re.compile("(.*) was buffeted by the sandstorm!")
+burn_dmg_pat = re.compile("(.*) was hurt by its burn!")
+poison_dmg_pat = re.compile("(.*) was hurt by poison!")
+sandstorm_dmg_pat = re.compile("(.*) (was|is) buffeted by the sandstorm!")
 leftovers_pat = re.compile("(.*) restored a little HP using its Leftovers!")
 
 move_used_lines = []
@@ -331,6 +331,33 @@ for line_num, line in enumerate(log_arr):
                 mon.damage(12.5)
                 status = mon.space_status()
                 converted = rf'|-damage|p{player + 1}a: {mon.nick}|{mon.hp}\/100|[from] Stealth Rock'
+
+    elif sandstorm_dmg_pat.match(line):
+        match = sandstorm_dmg_pat.search(line)
+        if match:
+            player = -1
+            mon = None
+            if ('The foe\'s' in match.group(1)):
+                # the Foe
+                mon = players[other_player].currentmon
+                player = other_player
+
+            elif (f'{players[0]}\'s' in match.group(1)):
+                # Player 1
+                mon = players[0].currentmon
+                player = 0
+            elif (f'{players[1]}\'s' in match.group(1)):
+                # Player 2
+                mon = players[1].currentmon
+                player = 1
+            else:
+                mon = players[current_player].currentmon
+                player = current_player
+            if mon:
+                mon.damage(6.25)
+                status = mon.space_status()
+                converted = rf'|-damage|p{player + 1}a: {mon.nick}|{mon.hp}\/100|[from] Sandstorm'
+
 
     elif leftovers_pat.match(line):
         pokemon_healed = line.replace(" restored a little HP using its Leftovers!", "")
