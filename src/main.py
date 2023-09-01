29a6b8b0a1a061e2f5b23ffe1c512f7ffc08b86a
Red448 <arthury5519@gmail.com>
2023-08-31 23:18:39 -0400

    Implementing taunt

--- src/main.py
+++ src/main.py
@@ -143,7 +143,6 @@ def analyze_line(line: str) -> str:
     paralysis_pat = re.compile("(.*) is paralyzed! It may be unable to move!")
 
     encore_pat = re.compile("(.*) received an encore!")
-    encore_end_pat = re.compile("(.*) encore ended!") #couldn't find a log with encore ending so this may be changed
     taunt_pat = re.compile("(.*) fell for the taunt!")
     taunt_end_pat = re.compile("(.*) taunt ended!")
 
@@ -176,7 +175,6 @@ def analyze_line(line: str) -> str:
     crit_pat = "A critical hit!"
     super_effective_pat = "It's super effective!"
     not_very_effective_pat = "It's not very effective..."
-    #but_it_failed_pat = "But it failed!" #Happens when trying to toxic a poison type, double protecting, etc
 
     converted = '|'
     if battle_started_pat.match(line):
@@ -356,10 +354,6 @@ def analyze_line(line: str) -> str:
     elif immune_pat.match(line):
         move, use_player, target_player, use_mon, target_mon = moves_buffer
         converted = f'|-immune|p{target_player+1}a: {target_mon.nick}'
-
-    #elif but_it_failed_pat == line:
-    #    move, use_player, target_player, use_mon, target_mon = moves_buffer
-    #    converted = f'|-fail|p{target_player}a: {target_mon.nick}'
 
     elif damage_dealt_pat.search(line):
         move, use_player, target_player, use_mon, target_mon = moves_buffer
@@ -601,12 +595,6 @@ def analyze_line(line: str) -> str:
         mon = players[player].currentmon
         if mon:
             converted = f'|-start|p{player + 1}a: {mon.nick}|Encore'
-
-    elif encore_end_pat.match(line):
-        player = identify_player(line, encore_end_pat)
-        mon = players[player].currentmon
-        if mon:
-            converted = f'|-end|p{player + 1}a: {mon.nick}|Encore'
 
     elif taunt_pat.match(line):
         player = identify_player(line, taunt_pat)
